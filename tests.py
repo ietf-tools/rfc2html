21521cdc677396e4b4a6b53450480a8ad7c42099
Robert Sparks <rjsparks@nostrum.com>
2022-01-18 16:56:25 -0600

    fix:
    * ci: add test-pypi-publish workflow
    
    * ci: fix test pypi publish workflow
    
    * ci: fix test-pypi-publish workflow (2)
    
    * ci: fix test-pypi-publish workflow (3)
    
    * ci: fix test-pypi-publish workflow (4)
    
    * ci: add codeql workflow for PRs
    
    * ci: add unit testing check
    
    * ci: remove main from pr-checks workflow
    
    * docs: add CONTRIBUTING.md
    
    * docs: udpate README header + logo
    
    * docs: reformat README body
    
    * feat: Recognize draft references in two-column title block
    
      * Adjust split-line draft name regex to allow text after 3+ spaces
      * Add test cases
      * Import unittest when running tests.py directly
    
    * doc: update history and links in README.md
    
    * chore: add Trust copyright, update setup metadata, correct tox, address flake8s
    
    * ci: replace pr unittext with tox
    
    * chore: add gh-actions config for tox
    
    * ci: update pr-checks workflow for parallel tox
    
    * ci: fix tests invocation warning
    
    * ci: add platform matrix to pr-checks tox
    
    * ci: update tox config to use provided platform
    
    Co-authored-by: Nicolas Giard <github@ngpixel.com>
    Co-authored-by: Jennifer Richards <jennifer@painless-security.com>

--- tests.py
+++ tests.py
@@ -21,5 +21,23 @@ class MarkupTestCase(TestCase):
         html = markup('text RFC1234 text')
         self.assertEqual(html, '<pre>text <a href="./rfc1234">RFC1234</a> text</pre>')
 
+    def test_draft_ref_with_linebreak(self):
+        html = markup('draft-ietf-\n              some-name-00')
+        self.assertEqual(
+            html,
+            '<pre>{open_tag}draft-ietf-</a>\n              {open_tag}some-name-00</a></pre>'.format(
+                open_tag='<a href="./draft-ietf-some-name-00">',
+            ))
+
+    def test_draft_ref_with_linebreak_in_header(self):
+        html = markup('draft-ietf-   S. Author\n              some-name-00   Org')
+        self.assertEqual(
+            html,
+            '<pre>{open_tag}draft-ietf-</a>   S. Author\n              {open_tag}some-name-00</a>   Org</pre>'.format(
+                open_tag='<a href="./draft-ietf-some-name-00">',
+            ))
+
+
 if __name__ == '__main__':
+    import unittest
     unittest.main()
